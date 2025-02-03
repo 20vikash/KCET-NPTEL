e9799a81316f22b7d9698fe6289253ee75ce0a92
ssvikash_20 <arisivikash@gmail.com>
2025-02-03 07:44:56 +0530

    You gitignored everything stupid

---
+++ video_upload_service/clients/minio/client.go
@@ -0,0 +1,32 @@
+package minio
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "video_upload/grpc/minio"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func GetResponse() string {
+	conn, err := grpc.NewClient("localhost:6969", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("failed to connect to gRPC server at localhost:50051: %v", err)
+	}
+
+	defer conn.Close()
+	c := pb.NewHelloWorldServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	r, err := c.SayHello(ctx, &pb.HelloWorldRequest{})
+	if err != nil {
+		log.Fatalf("error calling function SayHello: %v", err)
+	}
+
+	return r.GetMessage()
+}
