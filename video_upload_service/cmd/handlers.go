a21ce32fbbf953cc8a94390a1b081fd47b783459
ssvikash_20 <arisivikash@gmail.com>
2025-02-03 23:50:13 +0530

    Handle upload video (chunks)

--- video_upload_service/cmd/handlers.go
+++ video_upload_service/cmd/handlers.go
@@ -1,16 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func rootPage(w http.ResponseWriter, r *http.Request) {
 	// res := minio.GetResponse()
 	// fileName := r.Header.Get("file-name")
+
+	home, _ := os.UserHomeDir()
+	filePath := filepath.Join(home, "Desktop", "videos", "image.png")
+
+	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Println("Cannot open the file")
+	}
+
+	defer file.Close()
+
 	body := r.Body
 
-	fmt.Println(body)
+	_, err = io.Copy(file, body)
+	if err != nil {
+		log.Println("Error. yea.. pretty much")
+	}
 
-	// w.Write([]byte(res))
+	w.Write([]byte("Success"))
 }
