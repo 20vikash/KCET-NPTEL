cb4e99f6f0cace9f0576774b1a45493f166633d7
ssvikash_20 <arisivikash@gmail.com>
2025-02-08 23:28:59 +0530

    Use ffmpeg to convert media files to HLS files

--- video_upload_service/cmd/handlers.go
+++ video_upload_service/cmd/handlers.go
@@ -7,17 +7,26 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 )
 
 func rootPage(w http.ResponseWriter, r *http.Request) {
+	done := r.Header.Get("done")
+	isDone, _ := strconv.ParseBool(done)
+
 	home, _ := os.UserHomeDir()
-	filePath := filepath.Join(home, "Desktop", "videos", "image.png")
+	filePath := filepath.Join(home, "Desktop", "videos", "image.mov")
 	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Println("Cannot open the file")
 	}
 	defer file.Close()
 	body := r.Body
+
+	if isDone {
+		ConvertToHls(filePath)
+		log.Println("Done is true")
+	}
 
 	fmt.Println(body)
 	_, err = io.Copy(file, body)
