369e3178cf2594c0a5b5d7efa5350ed42ed803fa
ssvikash_20 <arisivikash@gmail.com>
2025-02-17 19:25:48 +0530

    Add gRPC client code

---
+++ video_upload_service/grpc/client/auth/client.go
@@ -0,0 +1,20 @@
+package auth
+
+import (
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func ConnectToAuth() AuthServiceClient {
+	conn, err := grpc.NewClient("localhost:5001", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Panic("Failed to connect to auth service")
+	}
+	defer conn.Close()
+
+	c := NewAuthServiceClient(conn)
+
+	return c
+}
