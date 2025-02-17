d68bede0a45d9ca767feff2a25735804c40430ce
ssvikash_20 <arisivikash@gmail.com>
2025-02-17 14:13:54 +0530

    Initialize grpc for authentication

--- authentication_service/cmd/main.go
+++ authentication_service/cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	pb "authentication/grpc/server/auth"
+	"context"
+)
+
+type Server struct {
+	pb.UnimplementedHelloWorldServiceServer
+}
+
 func main() {
 
+}
+
+func (s *Server) SayHello(ctx context.Context, in *pb.HelloWorldRequest) (*pb.HelloWorldResponse, error) {
+	return &pb.HelloWorldResponse{Message: "Hello, World! "}, nil
 }
