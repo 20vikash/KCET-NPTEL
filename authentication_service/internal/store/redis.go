7d9c94bcccd57f1709752d067d855660cad5d243
ssvikash_20 <arisivikash@gmail.com>
2025-02-18 12:03:21 +0530

    Connect to redis datastore

--- authentication_service/internal/store/redis.go
+++ authentication_service/internal/store/redis.go
@@ -1,1 +1,16 @@
 package store
+
+import "github.com/redis/go-redis/v9"
+
+type RedisStore struct {
+}
+
+func Connect() *redis.Client {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	return rdb
+}
