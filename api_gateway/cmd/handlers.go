df4b6adbf5d6d31c7359c57f7c01cc84f9a54f02
ssvikash_20 <arisivikash@gmail.com>
2025-02-21 22:44:59 +0530

    UploadVideo handler

--- api_gateway/cmd/handlers.go
+++ api_gateway/cmd/handlers.go
@@ -1,15 +1,40 @@
 package main
 
-import "net/http"
+import (
+	"bytes"
+	"context"
+	"log"
+	"net/http"
+
+	video "gateway/grpc/client/video"
+)
 
 func (app *Application) Hello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello world"))
 }
 
-func (app *Application) UploadVideo(w http.ResponseWriter, r *http.Request) {
+func (app *Application) UploadVideo(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+	done := r.Header.Get("done")
+	data := r.Body
 
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(data)
+	dataBytes := buf.Bytes()
+
+	videoData := &video.VideoData{
+		Data: dataBytes,
+		Done: done,
+	}
+
+	_, err := app.VideoService.UploadBinary(ctx, videoData)
+	if err != nil {
+		log.Println(err)
+		w.Write([]byte("Success"))
+	} else {
+		w.Write([]byte("Failed"))
+	}
 }
 
-func (app *Application) CreateUser(w http.ResponseWriter, r *http.Request) {
+func (app *Application) CreateUser(ctx context.Context, w http.ResponseWriter, r *http.Request) {
 
 }
