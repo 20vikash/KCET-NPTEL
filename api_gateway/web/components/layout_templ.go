680c5fd5c27adbb7cfa892203c16744b64904038
ssvikash_20 <arisivikash@gmail.com>
2025-03-05 14:23:16 +0530

    Add hx-ext=json-enc to send the hx post request's content type as application/json and not encoded form

--- api_gateway/web/components/layout_templ.go
+++ api_gateway/web/components/layout_templ.go
@@ -29,7 +29,7 @@ func Layout(content templ.Component) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>My App</title><script src=\"https://cdnjs.cloudflare.com/ajax/libs/htmx/2.0.4/htmx.min.js\"></script></head><body>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>My App</title><script src=\"https://cdnjs.cloudflare.com/ajax/libs/htmx/2.0.4/htmx.min.js\"></script><script src=\"https://unpkg.com/htmx.org/dist/ext/json-enc.js\"></script></head><body>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
