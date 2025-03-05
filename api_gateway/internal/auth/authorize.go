b6168639c96334fbc8a714412439b88a8cf2c681
ssvikash_20 <arisivikash@gmail.com>
2025-03-05 11:32:06 +0530

    IsRole function to check the authorization level of a user

--- api_gateway/internal/auth/authorize.go
+++ api_gateway/internal/auth/authorize.go
@@ -1,19 +1,24 @@
 package authorize
 
-import "github.com/alexedwards/scs/v2"
+import (
+	"context"
+	model "gateway/models/auth"
+
+	"github.com/alexedwards/scs/v2"
+)
 
 type Authorize struct {
 	Session *scs.SessionManager
 }
 
-func (a *Authorize) IsStudent() {
+func (a *Authorize) IsRole(ctx context.Context, role string) (*model.UserSession, bool) {
+	role_data := a.Session.Get(ctx, "role").(string)
+	user_name_data := a.Session.Get(ctx, "user_name").(string)
+	id_data := a.Session.Get(ctx, "id").(string)
 
-}
+	if role_data != role {
+		return &model.UserSession{}, false
+	}
 
-func (a *Authorize) IsTeacher() {
-
-}
-
-func (a *Authorize) IsAdmin() {
-
+	return &model.UserSession{UserName: user_name_data, Id: id_data, Role: role_data}, true
 }
