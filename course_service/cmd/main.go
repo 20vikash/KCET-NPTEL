ce398798ffd24d3d82d73f70c364fcc64ba08452
ssvikash_20 <arisivikash@gmail.com>
2025-03-06 19:12:46 +0530

    Add store to the main file

--- course_service/cmd/main.go
+++ course_service/cmd/main.go
@@ -1,21 +1,40 @@
 package main
 
 import (
+	"context"
+	env "course"
 	pb "course/grpc/server"
+	"course/internal/store"
 	"log"
 	"net"
+
+	"course/internal/db"
 
 	"google.golang.org/grpc"
 )
 
 type Application struct {
 	pb.UnimplementedCourseServiceServer
-	Port string
+	Port  string
+	Store *store.Store
 }
 
 func main() {
+	pg := &db.PG{
+		Host:     "postgres_db",
+		Username: env.GetDBUserName(),
+		Password: env.GetDBPassword(),
+		Database: env.GetDBName(),
+	}
+
+	db, err := pg.Connect(context.Background())
+	if err != nil {
+		log.Println(err)
+	}
+
 	app := &Application{
-		Port: ":5004",
+		Port:  ":5004",
+		Store: store.NewStore(db),
 	}
 
 	lis, err := net.Listen("tcp", app.Port)
