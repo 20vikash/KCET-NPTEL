07d40cecba08af14f137896e8e00a41af59efeee
ssvikash_20 <arisivikash@gmail.com>
2025-03-06 10:26:41 +0530

    Add SQL relations

--- init.sql
+++ init.sql
@@ -9,3 +9,39 @@ CREATE TABLE auth (
     created_at TIMESTAMP DEFAULT now(),
     role VARCHAR(10) DEFAULT 'student' CHECK (role IN ('student', 'teacher', 'admin'))
 );
+
+CREATE TABLE course (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    description TEXT NOT NULL,
+    thumbnail VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT now()
+);
+
+CREATE TABLE course_teacher (
+    id SERIAL PRIMARY KEY,
+    course_id INTEGER REFERENCES course(id) ON DELETE CASCADE,
+    teacher_id INTEGER REFERENCES auth(id) ON DELETE CASCADE
+);
+
+CREATE TABLE enrollment (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES auth(id) ON DELETE CASCADE,
+    course_id INTEGER REFERENCES course(id) ON DELETE CASCADE,
+    enrolled_at TIMESTAMP DEFAULT now()
+);
+
+CREATE TABLE section (
+    id SERIAL PRIMARY KEY,
+    course_id INTEGER REFERENCES course(id) ON DELETE CASCADE,
+    section_number INTEGER NOT NULL CHECK (section_number > 0),
+    title VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE video (
+    id SERIAL PRIMARY KEY,
+    section_id INTEGER REFERENCES section(id) ON DELETE CASCADE,
+    title VARCHAR(100) NOT NULL,
+    video_url TEXT NOT NULL,
+    duration INTEGER NOT NULL CHECK (duration > 0)
+);
